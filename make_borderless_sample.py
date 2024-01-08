74bed037c97390b7845775d267e56c59717353a4
SiamSaleh <siamsaleh9@gmail.com>
2024-01-08 18:51:36 +0600

    borderless images added

--- make_borderless_sample.py
+++ make_borderless_sample.py
@@ -13,13 +13,13 @@ for i in range(0, image_count):
     h, w = img.shape
 
     # Top
-    cv2.line(img, (0, 0), (w, 0), (255, 255, 255), 80)
+    cv2.line(img, (0, 0), (w, 0), (242, 242, 242), 100)
     # LEFT
-    cv2.line(img, (0, 0), (0, h), (255, 255, 255), 80)
+    cv2.line(img, (0, 0), (0, h), (242, 242, 242), 100)
     # RIGHT
-    cv2.line(img, (w, 0), (w, h), (255, 255, 255), 80)
+    cv2.line(img, (w, 0), (w, h), (242, 242, 242), 100)
     # BOTTOM
-    cv2.line(img, (0, h), (w, h), (255, 255, 255), 80)
+    cv2.line(img, (0, h), (w, h), (242, 242, 242), 100)
 
     cv2.imwrite('borderless_sample/' + save_file_name, img)
     print('index - {:03d}\n'.format(i))
