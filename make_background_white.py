8defb3e725a11233286c56a34a94b3c13e118967
SiamSaleh <siamsaleh9@gmail.com>
2024-01-08 18:12:05 +0600

    white backgroud sample created

--- make_background_white.py
+++ make_background_white.py
@@ -1,32 +1,16 @@
 import support_class
 import os
 
-save_file_names = []  # all image names
 all_file_names = support_class.get_file_names("sample/", "")
+all_file_names = sorted(all_file_names)
 image_count = len(all_file_names)
 print(image_count)
 
-count = 1
-index = 0
 for i in range(0, image_count):
     save_file_name = 'page-{0:03}.png'.format(i + 1)
-    image_path = 'sample/' + save_file_name
+    image_path = 'sample/' + all_file_names[i]
 
-    string = 'python3 utils/make_white.py "sample/' + save_file_name + '" '
+    string = 'python3 utils/make_white.py "' + image_path + '" '
     string = string + '"white_sample/' + save_file_name + '"'
     os.system(string)
-    count = count + 1
-    index = index + 1
-    print(string + ' index - {:03d}\n'.format(index))
-
-
-# img = cv2.imread('sample/page-600.png', 0)
-# h, w = img.shape
-# cv2.line(img, (0, 0), (w, 0), (255, 255, 255), 30)
-# cv2.imshow("LSD", img)
-# cv2.waitKey(0)
-
-# img = cv2.imread(image_path, 0)
-# h, w = img.shape
-# cv2.line(img, (0, 0), (w, 0), (0, 0, 255), 25)
-
+    print(string + ' index - {:03d}\n'.format(i + 1))
