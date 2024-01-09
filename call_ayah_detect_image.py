85b8e3a79ea5d0f274479b9b6da338f41486bce6
SiamSaleh <siamsaleh9@gmail.com>
2024-01-09 11:44:42 +0600

    ayah detect image create

--- call_ayah_detect_image.py
+++ call_ayah_detect_image.py
@@ -28,32 +28,32 @@ all_file_names = support_class.get_file_names("img/surah_border_less_sample/", "
 image_count = len(all_file_names)
 print(image_count)
 
-# next_page_start = 1
-# for i in range(0, image_count):
-#     save_file_name = 'page-{0:03}.png'.format(i + 1)
-#     image_path = 'img/surah_border_less_sample/' + save_file_name
-#     uniq_detected_points, end_index = (
-#         detect_ayah.page_ayah_detect(image_path, next_page_start, save_file_name))
-#     next_page_start = end_index
-#
-# print(next_page_start)
+next_page_start = 13
+for i in range(0, image_count):
+    save_file_name = 'page-{0:03}.png'.format(i + 1)
+    image_path = 'img/surah_border_less_sample/' + save_file_name
+    uniq_detected_points, end_index = (
+        detect_ayah.page_ayah_detect(image_path, next_page_start, save_file_name))
+    next_page_start = end_index
+
+print(next_page_start)
 ########################################################################################################################
 # TODO page info detect
-index = 1
-position = 1
-start_page = 1
-end_page = 569
-for page in range(start_page, end_page + 1):
-    print(page)
-    save_file_name = 'page-{0:03}.png'.format(page)
-    image_path = 'img/surah_border_less_sample/' + save_file_name
-
-    surah_no, start_ayah_number = index_to_data.get_surah_ayah_no(index)
-    page_info = PageInfo(page, surah_no, start_ayah_number)
-    dataFrame = save_csv(page_info, dataFrame)
-
-    end_index = detect_ayah.page_info_method(image_path, index, "test.png")
-    index = end_index
-
-# save CSV file
-dataFrame.to_csv("page_info.csv", index=True)
+# index = 1
+# position = 1
+# start_page = 1
+# end_page = 605
+# for page in range(start_page, end_page + 1):
+#     print(page)
+#     save_file_name = 'page-{0:03}.png'.format(page)
+#     image_path = 'img/surah_border_less_sample/' + save_file_name
+#
+#     surah_no, start_ayah_number = index_to_data.get_surah_ayah_no(index)
+#     page_info = PageInfo(page, surah_no, start_ayah_number)
+#     dataFrame = save_csv(page_info, dataFrame)
+#
+#     end_index = detect_ayah.page_info_method(image_path, index, "test.png")
+#     index = end_index
+#
+# # save CSV file
+# dataFrame.to_csv("page_info.csv", index=True)
--- detect_ayah.py
+++ detect_ayah.py
@@ -23,15 +23,15 @@ def get_all_detected_point(loc):
             # add new circle all points
             all_point_list = all_point_list + support_class.points_in_circle_np(25, point[0], point[1])
             # TODO uncomment
-            # print(point)
+            print(point)
 
             # print(all_point_list)
 
     # TODO uncomment
-    # print(uniq_detected_points)
-    # print("All points - {:d}".format(len(detected_points)))
-    # print("Uniq ayah/points - {:d}".format(len(uniq_detected_points)))
-    # print("##########################################################")
+    print(uniq_detected_points)
+    print("All points - {:d}".format(len(detected_points)))
+    print("Uniq ayah/points - {:d}".format(len(uniq_detected_points)))
+    print("##########################################################")
     # [D] [1st priroty A]
     uniq_detected_points = sorted(uniq_detected_points, key=lambda x: (x[1], -x[0]))
     return uniq_detected_points
@@ -65,16 +65,16 @@ def template_matching(image_path, start_index, save_file_name):
         start_index = start_index + 1
 
     # TODO
-    # cv2.imwrite('img/ayah_detect_final/' + save_file_name, img_rgb)
+    cv2.imwrite('img/ayah_detect_final/' + save_file_name, img_rgb)
     # cv2.imwrite(save_file_name, img_rgb)
     #
     # cv2.imshow("Matched image", img_rgb)
     # cv2.waitKey()
     # cv2.destroyAllWindows()
 
-    # print(image_path)
-    # print(start_index)
-    # print("###########################################################################################################")
+    print(image_path)
+    print(start_index)
+    print("###########################################################################################################")
     return uniq_detected_points, start_index
 
 
